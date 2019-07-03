4e81fed513b1c3430aec128491c35209bade93b4
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-03 19:53:18 +0000

    Code Formatted

--- src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
+++ src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
@@ -1,4 +1,41 @@
 package ca.jrvs.apps.twitter.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "type",
+        "coordinates"
+})
 public class Coordinates {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("coordinates")
+    private List<Double> coordinates = null;
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("coordinates")
+    public List<Double> getCoordinates() {
+        return coordinates;
+    }
+
+    @JsonProperty("coordinates")
+    public void setCoordinates(List<Double> coordinates) {
+        this.coordinates = coordinates;
+    }
+
 }
