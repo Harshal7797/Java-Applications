c17b9c4f386306a34d818c1964eb7504a23c4add
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-03 18:46:23 +0000

    Twiiter CLI update

---
+++ src/main/java/ca/jrvs/apps/twitter/example/JsonParser/JasonParser.java
@@ -0,0 +1,72 @@
+package ca.jrvs.apps.twitter.example.JsonParser;
+
+import ca.jrvs.apps.twitter.example.JsonParser.dto.Company;
+import ca.jrvs.apps.twitter.example.JsonParser.dto.Dividend;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.org.apache.xpath.internal.operations.Div;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JasonParser <T> {
+
+    /**
+     *Convert a java object to JSON string
+     * @param object input object
+     * @return JSON String
+     * @throws JsonProcessingException
+     */
+
+    public static String toJson (Object object, boolean prettyJson, boolean includeNullValues) throws JsonProcessingException {
+
+        try{
+            ObjectMapper mapper =new ObjectMapper();
+        mapper.writeValue(new File("/home/centos/dev/jrvs/bootcamp/java_apps/result.json"), object);
+        String json= mapper.writeValueAsString(object);
+            return json;
+        }catch (IOException x){
+            x.printStackTrace();
+            throw new RuntimeException(x);
+        }
+    }
+
+    /**
+     * parse JSON string to a object
+     * @param json
+     * @param clazz object class
+     * @param <T> Type
+     * @return object
+     * @throws IOException
+     */
+
+    public static <T> T toObjectFromJson(String json, Class clazz)throws IOException{
+       try {
+           ObjectMapper mapper = new ObjectMapper();
+           Dividend cp = (Dividend) mapper.readValue(json, clazz);
+           System.out.println("Printing CP");
+           System.out.println(cp);
+           return (T) cp;
+       }catch (IOException e){
+           e.printStackTrace();
+           throw new RuntimeException(e);
+       }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Dividend dividend=new Dividend();
+        dividend.setExDate("2018-02-09");
+        dividend.setPaymentDate("2018-02-15");
+        dividend.setRecordDate("2018-02-01");
+        dividend.setDeclaredDate("2018-02-01");
+        dividend.setAmount(0.63);
+        Dividend dividend1=new Dividend();
+        String test= toJson(dividend,true,true);
+
+JasonParser <Dividend>  xx= new JasonParser();
+               Dividend xxx =  toObjectFromJson(test,Dividend.class);
+
+
+    }
+    }
---
+++ src/main/java/ca/jrvs/apps/twitter/example/JsonParser/dto/Financial.java
@@ -0,0 +1,104 @@
+package ca.jrvs.apps.twitter.example.JsonParser.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "reportDate",
+        "grossProfit",
+        "costOfRevenue",
+        "operatingRevenue",
+        "totalRevenue",
+        "operatingIncome",
+        "netIncome"
+})
+public class Financial {
+
+    @JsonProperty("reportDate")
+    private String reportDate;
+    @JsonProperty("grossProfit")
+    private Long grossProfit;
+    @JsonProperty("costOfRevenue")
+    private Long costOfRevenue;
+    @JsonProperty("operatingRevenue")
+    private Long operatingRevenue;
+    @JsonProperty("totalRevenue")
+    private Long totalRevenue;
+    @JsonProperty("operatingIncome")
+    private Long operatingIncome;
+    @JsonProperty("netIncome")
+    private Long netIncome;
+
+    @JsonProperty("reportDate")
+    public String getReportDate() {
+        return reportDate;
+    }
+
+    @JsonProperty("reportDate")
+    public void setReportDate(String reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    @JsonProperty("grossProfit")
+    public Long getGrossProfit() {
+        return grossProfit;
+    }
+
+    @JsonProperty("grossProfit")
+    public void setGrossProfit(Long grossProfit) {
+        this.grossProfit = grossProfit;
+    }
+
+    @JsonProperty("costOfRevenue")
+    public Long getCostOfRevenue() {
+        return costOfRevenue;
+    }
+
+    @JsonProperty("costOfRevenue")
+    public void setCostOfRevenue(Long costOfRevenue) {
+        this.costOfRevenue = costOfRevenue;
+    }
+
+    @JsonProperty("operatingRevenue")
+    public Long getOperatingRevenue() {
+        return operatingRevenue;
+    }
+
+    @JsonProperty("operatingRevenue")
+    public void setOperatingRevenue(Long operatingRevenue) {
+        this.operatingRevenue = operatingRevenue;
+    }
+
+    @JsonProperty("totalRevenue")
+    public Long getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    @JsonProperty("totalRevenue")
+    public void setTotalRevenue(Long totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+
+    @JsonProperty("operatingIncome")
+    public Long getOperatingIncome() {
+        return operatingIncome;
+    }
+
+    @JsonProperty("operatingIncome")
+    public void setOperatingIncome(Long operatingIncome) {
+        this.operatingIncome = operatingIncome;
+    }
+
+    @JsonProperty("netIncome")
+    public Long getNetIncome() {
+        return netIncome;
+    }
+
+    @JsonProperty("netIncome")
+    public void setNetIncome(Long netIncome) {
+        this.netIncome = netIncome;
+    }
+
+}
