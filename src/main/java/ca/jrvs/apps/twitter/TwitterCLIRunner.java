f0c0aea24e181fdecf10eeae9b721e107f5c4f59
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-08 14:57:01 +0000

    Code Formatted

--- src/main/java/ca/jrvs/apps/twitter/TwitterCLIRunner.java
+++ src/main/java/ca/jrvs/apps/twitter/TwitterCLIRunner.java
@@ -2,6 +2,7 @@ package ca.jrvs.apps.twitter;
 
 import ca.jrvs.apps.twitter.service.TwitterService;
 
+
 public class TwitterCLIRunner {
     private TwitterService service;
 
@@ -10,5 +11,23 @@ public class TwitterCLIRunner {
     }
 
     public void run(String[] args) {
+        if (args.length < 2) {
+            throw new RuntimeException("USAGE: TwitterCLI post|show|deleteTweet");
+        }
+        switch (args[0].toLowerCase()) {
+            case "post":
+                String[] tmp = args[2].split(":");
+                service.postTweet(args[1], Double.parseDouble(tmp[0]), Double.parseDouble(tmp[1]));
+                break;
+            case "show":
+                service.showTweet(args[1], args[2].split(","));
+                break;
+            case "delete":
+                service.deleteTweets(args[1].split(","));
+                break;
+            default:
+                System.out.println("USAGE TwitterCLI post|show|delete");
+                break;
+        }
     }
 }
