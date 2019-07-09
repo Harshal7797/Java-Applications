da69233afd3aacd963224f049b0b73a19cab9c1e
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-09 19:18:06 +0000

    Code Formatted

--- src/main/java/ca/jrvs/apps/twitter/dao/CrdRepository.java
+++ src/main/java/ca/jrvs/apps/twitter/dao/CrdRepository.java
@@ -1,9 +1,26 @@
 package ca.jrvs.apps.twitter.dao;
 
 public interface CrdRepository<T, ID> {
+    /**
+     * Create an entity to the underlying storage
+     *
+     * @param entity entity that to be created
+     * @return created entity
+     */
     T create(T entity);
 
+    /**
+     * Find an entity by its id
+     * @param id  entity id
+     * @return
+     */
     T findById(ID id);
+
+    /**
+     * Delete an entity by its ID
+     * @param id of entity to be deleted
+     * @return deleted entity
+     */
 
     T deleteById(ID id);
 }
--- src/main/java/ca/jrvs/apps/twitter/service/TwitterService.java
+++ src/main/java/ca/jrvs/apps/twitter/service/TwitterService.java
@@ -1,9 +1,38 @@
 package ca.jrvs.apps.twitter.service;
 
 public interface TwitterService {
+
+    /**
+     * Post a Tweet along with a geo location
+     * Print Tweet JSON which returned by the Twitter API
+     *
+     * @param text      tweet text
+     * @param latitude  geo latitude
+     * @param longitude geo longitude
+     * @throws IllegalArgumentException if text exceed max number of allowed characters
+     *                                  or lat/long out of range
+     */
     void postTweet(String text, Double latitude, Double longitude);
 
+    /**
+     * Search a Tweet by id and print Tweet Object which returned by the Twitter API
+     *
+     * @param id tweet id
+     * @param fields print Tweet fields from this param. Print all field if empty
+     *
+     * @throws IllegalArgumentException if id or fields param is invalid
+     */
+
     void showTweet(String id, String[] fields);
+
+    /**
+     * Delete Tweet (s) by id(s)
+     * Print Tweet object(s) which returned by Twitter API\
+     *
+     * @param ids tweet IDs which will be deleted
+     *
+     * @throws IllegalArgumentException if one of the IDs is valid
+     */
 
     void deleteTweets(String[] ids);
 }
